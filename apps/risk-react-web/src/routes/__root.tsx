a04d7147fd86691b527ab02c92ed624daab29033
dmshvetsov <shvetsovdm@gmail.com>
2026-05-14 11:32:12 +0200

    risk-react-web: init app

---
+++ apps/risk-react-web/src/routes/__root.tsx
@@ -0,0 +1,46 @@
+/// <reference types="vite/client" />
+
+import type { ReactNode } from 'react'
+import '../styles.css'
+import {
+  HeadContent,
+  Outlet,
+  Scripts,
+  createRootRoute,
+} from '@tanstack/react-router'
+
+export const Route = createRootRoute({
+  head: () => ({
+    meta: [
+      { charSet: 'utf-8' },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1',
+      },
+      { title: 'risk-react-web' },
+    ],
+  }),
+  component: RootComponent,
+})
+
+function RootComponent() {
+  return (
+    <RootDocument>
+      <Outlet />
+    </RootDocument>
+  )
+}
+
+function RootDocument({ children }: Readonly<{ children: ReactNode }>) {
+  return (
+    <html>
+      <head>
+        <HeadContent />
+      </head>
+      <body>
+        {children}
+        <Scripts />
+      </body>
+    </html>
+  )
+}
---
+++ apps/risk-react-web/vite.config.ts
@@ -0,0 +1,8 @@
+import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+import tailwindcss from '@tailwindcss/vite'
+import viteReact from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  plugins: [tailwindcss(), tanstackStart(), viteReact()],
+})
